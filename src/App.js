26439f1ce49c79852a5210df7205325274dbaf5b
ariframaps <ariframa@student.ub.ac.id>
2024-04-02 20:11:49 +0700

    membuat add task form input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
 import './App.css';
 
 function App() {
   return (
     <div className="App container">
       <Header />
+      <AddTask />
     </div>
   );
 }
---
+++ src/components/AddTask.js
@@ -0,0 +1,9 @@
+
+export const AddTask = () => {
+    return (
+        <form className="input-group my-5 w-50">
+            <input type="text" className="form-control" placeholder="Add task" />
+            <button className="btn btn-success" type="submit">Add</button>
+        </form>
+    )
+}
